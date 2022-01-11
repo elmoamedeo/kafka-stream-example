20b7f1f6421e5223cfcb94e17e51ec8e8ca970b8
Amedeo Elmo <38795528+elmoamedeo@users.noreply.github.com>
2022-01-10 22:49:43 -0300

    Adds Kafka Stream entrypoint (#18)
    * Updates pom.xml
    
    Adds gson from google to deserialize and serialize PageView.
    
    * Adds Kafka Stream entrypoint

--- src/main/java/KafkaStreamExample.java
+++ src/main/java/KafkaStreamExample.java
@@ -1,11 +1,36 @@
+import entrypoint.PageViewToLocationStream;
 import infra.config.KafkaConfig;
+import org.apache.kafka.streams.KafkaStreams;
 
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 
 public class KafkaStreamExample {
 
     public static void main(String[] args) {
         final KafkaConfig kafkaConfig = new KafkaConfig();
         final Properties properties = kafkaConfig.executeConfigs();
+
+        final PageViewToLocationStream stream = new PageViewToLocationStream();
+        final KafkaStreams kafkaStreams = new KafkaStreams(stream.createStream().build(), properties);
+
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        try {
+            kafkaStreams.start();
+            latch.await();
+        } catch (final Throwable e) {
+            System.exit(1);
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread("pageview-location-stream") {
+            @Override
+            public void run() {
+                kafkaStreams.close();
+                latch.countDown();
+            }
+        });
+
+        System.exit(0);
     }
 }
